b92806886b11eb2b18cb43ec44aabdb8c285ca8a
Gilles Laurant <gillesl721@gmail.com>
2023-02-20 19:26:50 +0100

    Create Server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send;
+});
+// define routes here..
+
+const server = app.listen(2000, function () {
+  console.log("Node server is running..");
+});
